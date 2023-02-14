530a25704920730eb13f2f904e66ecea7ea97fc6
Yanis Guaye <yanis.guaye@epitech.eu>
2023-02-14 15:58:45 +0100

    Configure application light and dark themes

--- app/src/main/java/eu/epitech/reyditech/LoginActivity.kt
+++ app/src/main/java/eu/epitech/reyditech/LoginActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +23,7 @@ class LoginActivity : AppCompatActivity() {
     }
 }
 
+@Preview
 @Composable
 private fun LoginPage() {
     Theme {
