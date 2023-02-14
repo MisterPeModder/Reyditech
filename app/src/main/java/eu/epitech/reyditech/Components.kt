c793f92489d7362972077053fcef5c6c42b7d382
Yanis Guaye <yanis.guaye@epitech.eu>
2023-02-14 15:58:45 +0100

    Configure application light and dark themes

--- app/src/main/java/eu/epitech/reyditech/Components.kt
+++ app/src/main/java/eu/epitech/reyditech/Components.kt
@@ -5,16 +5,29 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 /** Reyditech dark theme. */
-private val darkColors = darkColors()
+private val darkColors = darkColors(
+    primary = Color(0xFF1E88E5), // blue_600
+    primaryVariant = Color(0xFF039BE5), // blue_600
+    secondary = Color(0xFF01579B), // light_blue_900
+    secondaryVariant = Color(0xFF311B92), // dark_purple_900
+    background = Color(0xFF212121), // grey_900
+)
 /** Reyditech light theme. */
-private val lightColors = lightColors()
+private val lightColors = lightColors(
+    primary = Color(0xFF1E88E5), // blue_600
+    primaryVariant = Color(0xFF039BE5), // blue_600
+    secondary = Color(0xFF01579B), // light_blue_900
+    secondaryVariant = Color(0xFF311B92), // dark_purple_900
+    background = Color(0xFFFFFFFF), // white
+)
 
 @Composable
 internal fun Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable ()-> Unit
+    content: @Composable () -> Unit
 ) {
     MaterialTheme(
         colors = if (darkTheme) darkColors else lightColors,
