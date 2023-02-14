a53c4a54c926daed9918ba3459370c279012c5ed
Yanis Guaye <yanis.guaye@epitech.eu>
2023-02-14 15:13:12 +0100

    Add basic Layout for login page

--- app/src/main/java/eu/epitech/reyditech/LoginActivity.kt
+++ app/src/main/java/eu/epitech/reyditech/LoginActivity.kt
@@ -1,26 +1,48 @@
 package eu.epitech.reyditech
 
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LockOpen
+import androidx.compose.material.icons.filled.Login
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Title("login")
+            LoginPage()
         }
     }
 }
 
 @Composable
-private fun Title(name: String) {
-    Text(
-        text = "Title of page $name",
-        color = MaterialTheme.colors.primary,
-        style = MaterialTheme.typography.h1
-    )
+private fun LoginPage() {
+    Theme {
+        Box(contentAlignment = Alignment.TopCenter, modifier = Modifier.fillMaxSize()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    Icons.Filled.LockOpen,
+                    contentDescription = stringResource(R.string.appLogo),
+                    modifier = Modifier.fillMaxSize(0.5f),
+                    tint = MaterialTheme.colors.secondary,
+                )
+                Button(onClick = { println("clicked login button") }) {
+                    Icon(
+                        Icons.Filled.Login,
+                        contentDescription = stringResource(R.string.loginButtonDescription),
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                    Text(stringResource(R.string.loginButton))
+                }
+            }
+        }
+    }
 }
