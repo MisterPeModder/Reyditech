3cc64581d6ca6a2a7c647dc210b07e80f723ebc6
Yanis Guaye <yanis.guaye@epitech.eu>
2023-03-12 03:17:45 +0100

    Extract harcoded strings to resources

--- app/src/main/java/eu/epitech/reyditech/Reyditech.kt
+++ app/src/main/java/eu/epitech/reyditech/Reyditech.kt
@@ -1,6 +1,7 @@
 package eu.epitech.reyditech
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Scaffold
@@ -24,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import eu.epitech.reyditech.components.Theme
 
 internal const val PACKAGE_NAME: String = "eu.epitech.reyditech"
 internal const val USER_AGENT: String = "android:eu.epitech.reyditech:v1.0.0 (by /u/MisterPeModder)"
@@ -36,117 +38,123 @@ internal const val USER_AGENT: String = "android:eu.epitech.reyditech:v1.0.0 (by
 @Composable
 internal fun Reyditech() {
     val navController = rememberNavController()
-    var searchparam by remember { mutableStateOf(TextFieldValue("")) }
-    Scaffold(topBar = {
-        TopAppBar(
-//                backgroundColor = MaterialTheme.colors.primaryVariant,
-            backgroundColor = Color(0xFF039BE5), modifier = Modifier.height(63.dp), title = {
+    var searchParam by remember { mutableStateOf(TextFieldValue("")) }
 
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+    Theme {
+        Scaffold(topBar = {
+            TopAppBar(backgroundColor = MaterialTheme.colors.primaryVariant,
+                modifier = Modifier.height(63.dp),
+                title = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.dark_logo),
+                            contentDescription = stringResource(R.string.appLogo),
+                            tint = Color.Unspecified
+                        )
+                        Text(text = "Reyditech")
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .background(Transparent)
+                                .padding(bottom = 2.dp),
+                            textStyle = androidx.compose.ui.text.TextStyle(fontSize = 15.sp),
+                            value = searchParam,
+                            onValueChange = {
+                                println(searchParam)
+                                Log.i("Reyditech", "Search changed to ${searchParam.text}")
+                                searchParam = it
+                            },
+                            label = { Text(text = stringResource(R.string.searchLabel)) },
+                            placeholder = { Text(text = stringResource(R.string.searchPlaceholder)) },
+                            leadingIcon = {
+                                Icon(
+                                    painter = painterResource(R.drawable.search),
+                                    contentDescription = stringResource(R.string.searchLabel),
+                                    tint = Color.Unspecified,
+                                    modifier = Modifier.width(20.dp)
+                                )
+                            },
+                        )
+                    }
+                })
+        },
+
+            content = {
+                NavHost(navController = navController, startDestination = "login") {
+                    composable("login") {
+                        LoginScreen(onLoginFinished = { navController.navigate("main") })
+                    }
+                    composable("main") {
+                        MainScreen(onReLogin = { navController.navigate("login") })
+                    }
+                }
+            }, bottomBar = {
+                BottomAppBar(
+                    backgroundColor = MaterialTheme.colors.secondaryVariant,
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.dark_logo),
-                        contentDescription = "Logo",
-                        tint = Color.Unspecified
-                    )
-                    Text(
-                        text = "Reyditech"
-
-                    )
-                    OutlinedTextField(
-                        modifier = Modifier
-                            .background(Transparent)
-                            .padding(bottom = 2.dp),
-                        textStyle = androidx.compose.ui.text.TextStyle(fontSize = 15.sp),
-                        value = searchparam,
-                        onValueChange = {
-                            println(searchparam)
-                            searchparam = it
-                        },
-                        label = { Text(text = stringResource(R.string.search)) },
-                        placeholder = { Text(text = "Search...") },
-                        leadingIcon = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Button(
+                            onClick = { navController.navigate("main") },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Transparent),
+                            modifier = Modifier
+                                .width(130.dp)
+                                .fillMaxHeight()
+                                .padding(0.dp)
+                                .background(Transparent)
+                        ) {
                             Icon(
-                                painter = painterResource(id = R.drawable.search),
-                                contentDescription = "search",
-                                tint = Color.Unspecified,
-                                modifier = Modifier.width(20.dp)
+                                painter = painterResource(id = R.drawable.home),
+                                contentDescription = stringResource(R.string.homeSection),
+                                tint = Color.Unspecified
                             )
-                        },
-                    )
+                        }
+                        Button(
+                            onClick = {
+                                Log.i(
+                                    "Reyditech", "go to 'hot'"
+                                ) /*navController.navigate("hot")*/
+                            },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Transparent),
+                            modifier = Modifier
+                                .width(130.dp)
+                                .fillMaxHeight()
+                                .padding(0.dp)
+                                .background(Transparent)
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.hot),
+                                contentDescription = stringResource(R.string.hotSection),
+                                tint = Color.Unspecified
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                Log.i(
+                                    "Reyditech", "go to 'profile'"
+                                ) /* navController.navigate("hot")*/
+                            },
+                            colors = ButtonDefaults.buttonColors(backgroundColor = Transparent),
+                            modifier = Modifier
+                                .width(130.dp)
+                                .fillMaxHeight()
+                                .padding(0.dp)
+//                            .background(Transparent)
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.profile),
+                                contentDescription = stringResource(R.string.profileSection),
+                                tint = Color.Unspecified
+                            )
+                        }
+                    }
+
                 }
             })
-    },
-
-        content = {
-            NavHost(navController = navController, startDestination = "login") {
-                composable("login") {
-                    LoginScreen(onLoginFinished = { navController.navigate("main") })
-                }
-                composable("main") {
-                    MainScreen(onReLogin = { navController.navigate("login") })
-                }
-            }
-        }, bottomBar = {
-            BottomAppBar(
-//                backgroundColor = MaterialTheme.colors.secondaryVariant,
-                backgroundColor = Color(0xFF311B92),
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Button(
-                        onClick = { navController.navigate("main") },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Transparent),
-                        modifier = Modifier
-                            .width(130.dp)
-                            .fillMaxHeight()
-                            .padding(0.dp)
-                            .background(Transparent)
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.home),
-                            contentDescription = "Home",
-                            tint = Color.Unspecified
-                        )
-                    }
-                    Button(
-                        onClick = { println("Profile") /*navController.navigate("hot")*/ },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Transparent),
-                        modifier = Modifier
-                            .width(130.dp)
-                            .fillMaxHeight()
-                            .padding(0.dp)
-                            .background(Transparent)
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.hot),
-                            contentDescription = "Hot",
-                            tint = Color.Unspecified
-                        )
-                    }
-                    Button(
-                        onClick = { println("Profile") /* navController.navigate("hot")*/ },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Transparent),
-                        modifier = Modifier
-                            .width(130.dp)
-                            .fillMaxHeight()
-                            .padding(0.dp)
-//                            .background(Transparent)
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.profile),
-                            contentDescription = "Profile",
-                            tint = Color.Unspecified
-                        )
-                    }
-                }
-
-            }
-        })
-
+    }
 }
